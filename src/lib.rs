9a10d27f90e58040a3a13156dd9a7fa9ee922613
AnneKitsune <annekitsunefox@gmail.com>
2025-02-08 18:30:10 -0500

    progress

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,23 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use nohash_hasher::BuildNoHashHasher;
+// Uuid -> vector idx
+type UuidMap = HashMap<u128, usize, BuildNoHashHasher<u128>>;
+
+pub struct Table<T> {
+    // Contains a map of uuid to data vector index.
+    map: UuidMap,
+    data: Vec<T>,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl<T> Table<T> {
+    pub fn new() -> Self {
+        let map = UuidMap::with_capacity_and_hasher(16, BuildNoHashHasher::default());
+        Self {
+            map,
+            data: vec![],
+        }
     }
+
+    // remove, count, values -> iterator, get, add(value) -> key, add_with_key(key, value)
+    // remove must use swapRemove in the vec and update the map
 }
+
